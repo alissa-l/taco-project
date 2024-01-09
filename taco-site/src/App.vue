acd6bbf2acee7f87991cd5e407536be350a1f2f0
alissa <alissa@nordestefomento.com.br>
2024-01-09 14:37:42 -0300

    Adding vue files

---
+++ taco-site/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <h1>oi</h1>
+  
+  <button @click="getData()">AAAAAAA</button>
+  
+</template>
+
+<script>
+
+//Se precisar dividir os repos
+//import tacoData from "./taco_data/taco2011.json";
+import tacoData from "../../data/taco2011.json";
+
+
+
+export default {
+    name: 'App',
+    data() {
+        return {
+            data1: [],
+            message: 'Hello World!'
+        }
+    },
+    methods: {
+        getData() {
+          let myDataArr = Array.from(tacoData['Data']);
+
+          
+
+          console.log(myDataArr)
+
+          
+
+        }
+    },    
+}
+
+</script>
