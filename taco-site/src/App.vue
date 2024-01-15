c0a253d9927eb5e70e3241f4372d9f3f11590342
alissa-l <alissa@nordestefomento.com.br>
2024-01-15 11:21:53 -0300

    Componentes vue

--- taco-site/src/App.vue
+++ taco-site/src/App.vue
@@ -1,7 +1,24 @@
 <template>
-  <h1>oi</h1>
-  
-  <button @click="getData()">AAAAAAA</button>
+
+
+  <div class="container">
+
+    <div class="row" v-for="chunk in alimentosChunked">
+
+
+
+      <div class="col" v-for="alimento in chunk">
+
+        <AlimentoComponent :alimentoObj="alimento" >
+        </AlimentoComponent>
+
+      </div>
+    
+    </div>
+
+
+  </div>
+
   
 </template>
 
@@ -10,27 +27,52 @@
 //Se precisar dividir os repos
 //import tacoData from "./taco_data/taco2011.json";
 import tacoData from "../../data/taco2011.json";
-
-
+import { Alimento } from "./classes/alimento.js";
 
 export default {
     name: 'App',
     data() {
         return {
-            data1: [],
-            message: 'Hello World!'
+          categorias: [],
+          alimentos: [],
+          alimentosChunked: [],
+          unidades: [],
         }
+    },
+    beforeMount() {
+      this.getData();
     },
     methods: {
         getData() {
-          let myDataArr = Array.from(tacoData['Data']);
 
+
+          let categorias = tacoData['Categorias'];
+          let myData = tacoData['Data'];
+          let unidades = tacoData['NutrientesUnidades'];
+
+          for (let i = 0; i < categorias.length; i++) {
+            let alimentosCategoria = myData[categorias[i]];
+
+            for (let j = 0; j < alimentosCategoria.length; j++) {
+              let alimento = Object.assign(new Alimento, alimentosCategoria[j]);
+              this.alimentos.push(alimento);
+            }
+          }
+
+          for (let i = 0; i < unidades.length; i++) {
+            let unidade = unidades[i];
+            this.unidades.push(unidade);
+          }
+
+          this.dataChunk(this.alimentos, 3);
           
-
-          console.log(myDataArr)
-
-          
-
+        },
+        dataChunk(array, size) {
+          let chunkedArray = [];
+          for (let i = 0; i < array.length; i += size) {
+            chunkedArray.push(array.slice(i, i + size));
+          }
+          this.alimentosChunked = chunkedArray;
         }
     },    
 }
---
+++ taco-site/src/components/AlimentoComponent.vue
@@ -0,0 +1,27 @@
+
+<template>
+
+    <h2 class="nomeAlimento">{{ alimento }}</h2>
+    <p>Kcal: {{ parseFloat(energia).toFixed(0) }}</p>
+    <p>Proteínas: {{ parseFloat(proteina).toFixed(0) }}</p>
+    <p>Carboidratos: {{ parseFloat(carboidrato).toFixed(0) }}</p>
+
+</template>
+
+<script>
+
+import { Alimento } from "../classes/alimento.js";
+
+export default {
+    name: 'Alimento',
+    props: {
+        alimentoObj: {
+            type: Object,
+        }
+    },
+    data() {
+        return this.alimentoObj;
+    }
+}
+
+</script>
