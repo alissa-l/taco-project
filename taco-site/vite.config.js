659b2b2a738bbe79228a548c6159c5aa474a623f
alissa-l <alissa@nordestefomento.com.br>
2024-01-15 11:49:57 -0300

    Testando deploy no github pages

--- taco-site/vite.config.js
+++ taco-site/vite.config.js
@@ -5,6 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' ? 
+  '/vuejs-gh-pages-gh-actions' : '/',
   plugins: [
     vue(),
   ],
