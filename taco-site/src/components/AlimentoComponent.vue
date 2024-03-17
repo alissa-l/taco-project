99f9f65ecc87dc11d059f7ef321d031d4c92a749
alissa-l <alissa.lima18@gmail.com>
2024-03-17 12:25:41 -0300

    frontend

--- taco-site/src/components/AlimentoComponent.vue
+++ taco-site/src/components/AlimentoComponent.vue
@@ -1,10 +1,38 @@
 
 <template>
 
-    <h2 class="nomeAlimento">{{ alimentoObj.nome }}</h2>
-    <p>Kcal: {{ parseFloat(energia).toFixed(0) }}</p>
-    <p>Proteínas: {{ parseFloat(proteina).toFixed(0) }}</p>
-    <p>Carboidratos: {{ parseFloat(carboidrato).toFixed(0) }}</p>
+    <div class="alimento" @click="mostrarDetalhes=!mostrarDetalhes">
+        <h2 class="nomeAlimento">{{ alimentoObj.nome }}</h2>
+        <p>Kcal: {{ parseFloat(alimentoObj.energia).toFixed(0) }}</p>
+        <p>Proteínas: {{ parseFloat(alimentoObj.proteina).toFixed(0) }}</p>
+        <p>Carboidratos: {{ parseFloat(alimentoObj.carboidrato).toFixed(0) }}</p>
+
+
+        <section v-if="mostrarDetalhes" class="alimento-infos-extras">
+            <p>Fibra Alimentar: {{ parseFloat(alimentoObj.alimentar).toFixed(0) }}</p>
+            <p>Ácido Graxo Saturado: {{ parseFloat(alimentoObj.agSaturado).toFixed(0) }}</p>
+            <p>Ácido Graxo Monoinsaturado: {{ parseFloat(alimentoObj.agMonoinsaturado).toFixed(0) }}</p>
+            <p>Ácido Graxo Poliinsaturado: {{ parseFloat(alimentoObj.agPoliinsaturado).toFixed(0) }}</p>
+            <p>Colesterol: {{ parseFloat(alimentoObj.colesterol).toFixed(0) }}</p>
+            <p>Cálcio: {{ parseFloat(alimentoObj.calcio).toFixed(0) }}</p>
+            <p>Magnésio: {{ parseFloat(alimentoObj.magnesio).toFixed(0) }}</p>
+            <p>Manganês: {{ parseFloat(alimentoObj.manganes).toFixed(0) }}</p>
+            <p>Fósforo: {{ parseFloat(alimentoObj.fosforo).toFixed(0) }}</p>
+            <p>Ferro: {{ parseFloat(alimentoObj.ferro).toFixed(0) }}</p>
+            <p>Sódio: {{ parseFloat(alimentoObj.sodio).toFixed(0) }}</p>
+            <p>Potássio: {{ parseFloat(alimentoObj.potassio).toFixed(0) }}</p>
+            <p>Cobre: {{ parseFloat(alimentoObj.cobre).toFixed(0) }}</p>
+            <p>Zinco: {{ parseFloat(alimentoObj.zinco).toFixed(0) }}</p>
+            <p>Retinol: {{ parseFloat(alimentoObj.retinol).toFixed(0) }}</p>
+            <p>RE: {{ parseFloat(alimentoObj.re).toFixed(0) }}</p>
+            <p>Tiamina: {{ parseFloat(alimentoObj.tiamina).toFixed(0) }}</p>
+            <p>Riboflavina: {{ parseFloat(alimentoObj.riboflavina).toFixed(0) }}</p>
+            <p>Piridoxina: {{ parseFloat(alimentoObj.piridoxina).toFixed(0) }}</p>
+            <p>Niacina: {{ parseFloat(alimentoObj.niacina).toFixed(0) }}</p>
+            <p>Vitamina C: {{ parseFloat(alimentoObj.vitaminac).toFixed(0) }}</p>
+            <p>Vitamina E: {{ parseFloat(alimentoObj.vitaminaE).toFixed(0) }}</p>
+        </section>
+    </div>
 
 </template>
 
@@ -19,7 +47,11 @@ export default {
         }
     },
     data() {
-        return this.alimentoObj;
+        return {
+            alimentoObj: this.alimentoObj,
+            mostrarDetalhes: false,
+
+        }
     }
 }
 
