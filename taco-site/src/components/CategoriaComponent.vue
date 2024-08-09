d95c617a61600374fc4a53ee298f877cb06c0cd5
Alissa Lima <alissa.lima18@gmail.com>
2024-08-09 19:46:30 -0300

    test

--- taco-site/src/components/CategoriaComponent.vue
+++ taco-site/src/components/CategoriaComponent.vue
@@ -2,8 +2,8 @@
 <template>
 
     <h2> {{ nome }} </h2>
-    <section class="md-container columns-3">
-        <AlimentoComponent v-for="aliment in alimentos" :alimentoObj="aliment" :mostrarDetalhes="false"></AlimentoComponent>
+    <section class="md-container columns-3 ">
+        <AlimentoComponent class="m-3" v-for="aliment in alimentos" :alimentoObj="aliment" :mostrarDetalhes="false"></AlimentoComponent>
     </section>
 
 </template>
